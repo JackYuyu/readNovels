9dbf9c9a812f27fff59a99ab8d026b7518b7ff3d
mac <mac@MacBook-Pro.local>
2020-04-23 10:26:22 +0800

    first

---
+++ readNovel-master/LookNovel/classes/reader/model/LNmodel.h
@@ -0,0 +1,19 @@
+//
+//  LNmodel.h
+//  LookNovel
+//
+//  Created by mac on 2020/4/22.
+//  Copyright © 2020 wcs Co.,ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LNmodel : NSObject
+@property (strong) NSString* cid;
+@property (strong) NSString* link;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ readNovel-master/LookNovel/classes/reader/model/LNmodel.m
@@ -0,0 +1,16 @@
+//
+//  LNmodel.m
+//  LookNovel
+//
+//  Created by mac on 2020/4/22.
+//  Copyright © 2020 wcs Co.,ltd. All rights reserved.
+//
+
+#import "LNmodel.h"
+
+@implementation LNmodel
++(NSDictionary *)mj_replacedKeyFromPropertyName{
+    return @{@"cid":@"id"};
+}
+
+@end
